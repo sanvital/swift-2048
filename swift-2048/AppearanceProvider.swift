6d15d017790258add0c77a30eefa6f775ee61976
Mark Sanvitale <mas@coradine.com>
2016-07-10 16:06:54 -0700

    Create the UIColors used in the display of the game board once and then reuse (via the type property mechanism).
    Potentially/probably wasteful to create a brand new UIColor every time we are asked for the color of a tile. Perhaps the system caches color objects, but better to not presume system behavior.

--- swift-2048/AppearanceProvider.swift
+++ swift-2048/AppearanceProvider.swift
@@ -15,34 +15,45 @@ protocol AppearanceProviderProtocol: class {
 }
 
 class AppearanceProvider: AppearanceProviderProtocol {
+  // Type properties for the UIColors used in this class. (language reference) "Stored type properties
+  // are lazily initialized on their first access. They are guaranteed to be initialized only once, even when 
+  // accessed by multiple threads simultaneously, and they do not need to be marked with the lazy modifier.
+  static let color2 = UIColor(red: 238.0/255.0, green: 228.0/255.0, blue: 218.0/255.0, alpha: 1.0)
+  static let color4 = UIColor(red: 237.0/255.0, green: 224.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+  static let color8 = UIColor(red: 242.0/255.0, green: 177.0/255.0, blue: 121.0/255.0, alpha: 1.0)
+  static let color16 = UIColor(red: 245.0/255.0, green: 149.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+  static let color32 = UIColor(red: 246.0/255.0, green: 124.0/255.0, blue: 95.0/255.0, alpha: 1.0)
+  static let color64 = UIColor(red: 246.0/255.0, green: 94.0/255.0, blue: 59.0/255.0, alpha: 1.0)
+  static let color128up = UIColor(red: 237.0/255.0, green: 207.0/255.0, blue: 114.0/255.0, alpha: 1.0)
+  static let textColor = UIColor(red: 119.0/255.0, green: 110.0/255.0, blue: 101.0/255.0, alpha: 1.0)
 
   // Provide a tile color for a given value
   func tileColor(value: Int) -> UIColor {
     switch value {
     case 2:
-      return UIColor(red: 238.0/255.0, green: 228.0/255.0, blue: 218.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color2
     case 4:
-      return UIColor(red: 237.0/255.0, green: 224.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color4
     case 8:
-      return UIColor(red: 242.0/255.0, green: 177.0/255.0, blue: 121.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color8
     case 16:
-      return UIColor(red: 245.0/255.0, green: 149.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color16
     case 32:
-      return UIColor(red: 246.0/255.0, green: 124.0/255.0, blue: 95.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color32
     case 64:
-      return UIColor(red: 246.0/255.0, green: 94.0/255.0, blue: 59.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color64
     case 128, 256, 512, 1024, 2048:
-      return UIColor(red: 237.0/255.0, green: 207.0/255.0, blue: 114.0/255.0, alpha: 1.0)
+      return AppearanceProvider.color128up
     default:
       return UIColor.whiteColor()
     }
   }
-
+  
   // Provide a numeral color for a given value
   func numberColor(value: Int) -> UIColor {
     switch value {
     case 2, 4:
-      return UIColor(red: 119.0/255.0, green: 110.0/255.0, blue: 101.0/255.0, alpha: 1.0)
+      return AppearanceProvider.textColor
     default:
       return UIColor.whiteColor()
     }
